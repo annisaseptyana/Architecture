347648f628337e5fc3fb240add58b564562c7b20
annisaseptyana <starrynyteskye@gmail.com>
2022-03-17 17:10:58 +0700

    Initialization

--- app/src/main/java/com/bangkit/architecture/MainActivity.kt
+++ app/src/main/java/com/bangkit/architecture/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.bangkit.architecture
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bangkit.architecture.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        mainBinding.btnViewmodel.setOnClickListener {
+            val moveToViewModel = Intent(this@MainActivity, ViewModel::class.java)
+            startActivity(moveToViewModel)
+        }
     }
 }
