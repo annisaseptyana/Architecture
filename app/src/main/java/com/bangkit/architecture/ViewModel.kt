5f879a838955ee9e9b1262811ccbece51dbb64a3
annisaseptyana <starrynyteskye@gmail.com>
2022-03-17 17:42:44 +0700

    ViewModel Exercise
    Parsing function from ViewModel class

--- app/src/main/java/com/bangkit/architecture/ViewModel.kt
+++ app/src/main/java/com/bangkit/architecture/ViewModel.kt
@@ -2,10 +2,47 @@ package com.bangkit.architecture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.bangkit.architecture.databinding.ActivityViewModelBinding
 
 class ViewModel : AppCompatActivity() {
+
+    private lateinit var binding: ActivityViewModelBinding
+    private val viewModel: MainViewModel by viewModels()
+
+    private fun displayResult() {
+        // Without LiveData, we need to update the result using this function
+        binding.tvResult.text = viewModel.result.toString()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_model)
+        binding = ActivityViewModelBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        displayResult()
+
+        binding.btnCalculate.setOnClickListener {
+            val width = binding.edtWidth.text.toString()
+            val height = binding.edtHeight.text.toString()
+            val length = binding.edtLength.text.toString()
+
+            when {
+                width.isEmpty() -> {
+                    binding.edtWidth.error = "This must be filled"
+                }
+                height.isEmpty() -> {
+                    binding.edtHeight.error = "This must be filled"
+                }
+                length.isEmpty() -> {
+                    binding.edtLength.error = "This must be filled"
+                }
+                else -> {
+                    viewModel.calculate(width, height, length)
+                    displayResult()
+                }
+            }
+        }
     }
 }
